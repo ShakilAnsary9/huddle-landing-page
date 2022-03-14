946b615a6764f24144a92762ae081998997c3043
Shakil Ansary <shakilansary9@gmail.com>
2022-03-14 20:31:46 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import './App.css';
+import bgDesktop from './images/bg-desktop.svg';
+import illustration from './images/illustration-mockups.svg';
+import logo from './images/logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage: `url(${bgDesktop})`
+    }}>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="content">
+        <div className="illustration">
+          <img src={illustration} alt="" />
+        </div>
+        <div className="text">
+          <h1>Build The Community <br/> Your Fans Will Love</h1>
+          <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.
+          </p>
+          <button>
+            Register
+          </button>
+        </div>
+      </div>
+      <div className="social">
+          <div className='icon'><FaFacebookF className='FaFacebook'/></div>
+          <div className='icon'><FaTwitter className='FaTwitter'/></div>
+          <div className='icon'><FaInstagram className='FaInstagram'/></div>
+      </div>
     </div>
   );
 }
